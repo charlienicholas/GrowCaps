69219497c9302d2d45f81f522da39b23575c8d38
charlienicholas <kangwibu129@gmail.com>
2026-05-09 18:15:14 +0700

    minggu keempat

---
+++ MingguEmpat/script.js
@@ -0,0 +1,14 @@
+let nama = "";
+let umur = 16;
+
+function inputNama() {
+    nama = document.querySelector("#namaInput".);
+}
+
+let kenalan = "Halo, nama saya " + nama + " dan umur saya " + umur + " tahun.";
+
+function sapa() {
+    alert(kenalan);
+}
+
+console.log(kenalan);
